c160bc68af4914a1e62dea2caa1821ce494f89ed
Spalo97 <giorgio.spalenza@gmail.com>
2019-04-16 13:29:27 +0200

    Update 2

--- CodiceFiscale2/src/it/unibs/eclisse/codiceFiscale2/Persona.java
+++ CodiceFiscale2/src/it/unibs/eclisse/codiceFiscale2/Persona.java
@@ -1,5 +1,28 @@
 package it.unibs.eclisse.codiceFiscale2;
 
 public class Persona {
-
+	
+	private int id;
+	private String nome;
+	private String cognome;
+	private char sesso;
+	private String comune_nascita;
+	private String data_nascita;
+	
+	public Persona() {
+		
+	}
+	
+	public Persona(int id,String firstName,String lastName,char gender,String birthplace,String birthday) {
+		this.id=id;
+		this.nome=firstName;
+		this.cognome=lastName;
+		this.sesso=gender;
+		this.comune_nascita=birthplace;
+		this.data_nascita=birthday;
+	}
+	
+	public String getFirstName() {
+		return nome;
+	}
 }
--- CodiceFiscale2/src/it/unibs/eclisse/codiceFiscale2/Support.java
+++ CodiceFiscale2/src/it/unibs/eclisse/codiceFiscale2/Support.java
@@ -1,15 +1,28 @@
 package it.unibs.eclisse.codiceFiscale2;
 
 import java.io.FileInputStream;
+import java.util.ArrayList;
 
 import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 
 public class Support {
 	
 	private String filename=null;
 	private XMLStreamReader xmlr = null;
+	private ArrayList<Persona> persone = new ArrayList();
+	private ArrayList<String> invalidi = new ArrayList();
+	private ArrayList<String> spaiati = new ArrayList();
 	
+	private int id;
+	private String nome;
+	private String cognome;
+	private char sesso;
+	private String comune_nascita;
+	private String data_nascita;
+ 	
 	public Support(String filename){
 		this.filename=filename;
 	}
@@ -24,5 +37,34 @@ public class Support {
 		}
 		
 		return this.xmlr;
+	}
+	
+	public void getAllFromXML() {
+		
+		try {
+			while (xmlr.hasNext()) { // continua a leggere finché ha eventi a disposizione
+				switch (xmlr.getEventType()) { // switch sul tipo di evento
+					case XMLStreamConstants.START_DOCUMENT: // inizio del documento: stampa che inizia il documento
+						break;
+					case XMLStreamConstants.START_ELEMENT: // inizio di un elemento: stampa il nome del tag e i suoi attributi
+						for (int i = 0; i < xmlr.getAttributeCount(); i++)
+							System.out.printf(" => attributo %s->%s%n", xmlr.getAttributeLocalName(i), xmlr.getAttributeValue(i));
+							
+						break;
+					case XMLStreamConstants.END_ELEMENT: // fine di un elemento: stampa il nome del tag chiuso
+						System.out.println("END-Tag " + xmlr.getLocalName()); break;
+					case XMLStreamConstants.COMMENT:
+						System.out.println("// commento " + xmlr.getText()); break; // commento: ne stampa il contenuto
+					case XMLStreamConstants.CHARACTERS: // content all’interno di un elemento: stampa il testo
+						if (xmlr.getText().trim().length() > 0) // controlla se il testo non contiene solo spazi
+						System.out.println("-> " + xmlr.getText());
+						break;
+					}
+				xmlr.next();
+			}
+		} catch (XMLStreamException e) {
+			System.out.println("Errore nella lettura del file " + filename);
+			System.out.println(e.getMessage());
+		}
 	}
 }
