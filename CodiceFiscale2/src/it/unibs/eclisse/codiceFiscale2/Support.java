01c37128b3ace21480856e6830e401e6f96e0170
Spalo97 <giorgio.spalenza@gmail.com>
2019-04-17 14:01:43 +0200

    Funzione getAllPersona

--- CodiceFiscale2/src/it/unibs/eclisse/codiceFiscale2/Support.java
+++ CodiceFiscale2/src/it/unibs/eclisse/codiceFiscale2/Support.java
@@ -33,6 +33,8 @@ public class Support {
 	private int mese;
 	private int giorno;
 	private String cod_fisc;
+	
+	private String tag;
 
 	private Persona p=new Persona();
 	
@@ -57,90 +59,54 @@ public class Support {
 		try {
 			xmlif = XMLInputFactory.newInstance();
 			xmlr = xmlif.createXMLStreamReader(filePersone, new FileInputStream(filePersone));
-			while (xmlr.hasNext()) { // continua a leggere finché ha eventi a disposizione
-				switch (xmlr.getEventType()) { // switch sul tipo di evento
-					case XMLStreamConstants.START_DOCUMENT: // inizio del documento: stampa che inizia il documento
+			
+			while (xmlr.hasNext()) {								
+				if(xmlr.getEventType()==XMLStreamConstants.START_ELEMENT) {
+					if(xmlr.getLocalName()=="persona") {
+						Persona p = new Persona();
+						persone.add(p);
+						persone.get(persone.size()-1).setId(xmlr.getAttributeValue(0));
+					}
+					switch(xmlr.getLocalName()) {
+					case "nome":
+						persone.get(persone.size()-1).setNome(xmlr.getElementText());
 						break;
-					case XMLStreamConstants.START_ELEMENT: // inizio di un elemento: stampa il nome del tag e i suoi attributi
-						for (int i = 0; i < xmlr.getAttributeCount(); i++) {
-							switch(xmlr.getAttributeLocalName(i)) {
-							case "persona": break;
-							case "nome": 	p.setNome(xmlr.getAttributeValue(i));
-											break;
-							case "cognome": p.setCognome(xmlr.getAttributeValue(i));
-											break;
-							case "sesso":	p.setSesso(xmlr.getAttributeValue(i));
-											break;
-							case "comune_nascita":  p.setComune_nascita(xmlr.getAttributeValue(i));
-													break;
-							case "data_nascita":	p.setData_nascita(xmlr.getAttributeValue(i));
-													if(!persone.add(p)) {
-														System.out.println("Errore!");
-													}
-													break;
-							}
-						}
+					case "cognome":
+						persone.get(persone.size()-1).setCognome(xmlr.getElementText());
 						break;
-					case XMLStreamConstants.END_ELEMENT: // fine di un elemento: stampa il nome del tag chiuso
-						//System.out.println("Fine lettura ed aggiunta dati da " + xmlr.getLocalName()); break;
-					case XMLStreamConstants.COMMENT:
-						System.out.println("// commento " + xmlr.getText()); break; // commento: ne stampa il contenuto
-					case XMLStreamConstants.CHARACTERS: // content all’interno di un elemento: stampa il testo
-						if (xmlr.getText().trim().length() > 0) // controlla se il testo non contiene solo spazi
-						//System.out.println("-> " + xmlr.getText());
+					case "sesso":
+						persone.get(persone.size()-1).setSesso(xmlr.getElementText());
+						break;
+					case "comune_nascita":
+						persone.get(persone.size()-1).setComune_nascita(xmlr.getElementText());
+						break;	
+					case "data_nascita":
+						persone.get(persone.size()-1).setData_nascita(xmlr.getElementText());
 						break;
 					}
-				
-				
+				}
 				xmlr.next();
-			}
-		} catch (XMLStreamException e) {
-			System.out.println("Errore nella lettura del file " + filePersone);
+				}
+		} catch (Exception e) {
+			System.out.println("Errore nell'inizializzazione del reader:");
 			System.out.println(e.getMessage());
 		}
 	}
 	
 	public void getAllCodici() {
 		
-		try {
-			xmlif = XMLInputFactory.newInstance();
-			xmlr = xmlif.createXMLStreamReader(fileCodici, new FileInputStream(fileCodici));
-			while (xmlr.hasNext()) { 
-				switch (xmlr.getEventType()) { 
-					case XMLStreamConstants.START_DOCUMENT:
-						System.out.println("Inizio lettura codici fiscali");
-						break;
-					case XMLStreamConstants.START_ELEMENT: 
-						for (int i = 0; i < xmlr.getAttributeCount(); i++) {
-									
-							}
-						}
-						break;
-					case XMLStreamConstants.END_ELEMENT: // fine di un elemento: stampa il nome del tag chiuso
-						//System.out.println("Fine lettura ed aggiunta dati da " + xmlr.getLocalName()); break;
-					case XMLStreamConstants.COMMENT:
-						System.out.println("// commento " + xmlr.getText()); break; // commento: ne stampa il contenuto
-					case XMLStreamConstants.CHARACTERS: // content all’interno di un elemento: stampa il testo
-						if (xmlr.getText().trim().length() > 0) // controlla se il testo non contiene solo spazi
-						//System.out.println("-> " + xmlr.getText());
-						break;
-					}
-				
-				
-				xmlr.next();
-			}
-		} catch (XMLStreamException e) {
-			System.out.println("Errore nella lettura del file " + fileCodici);
-			System.out.println(e.getMessage());
-		}
 	}
 	
-	public void test() {
+	/*public void test() {
 		int i=(int) (Math.random()*1000);
 		Persona c=persone.get(i);
-		String s= c.getNome();
-		System.out.println("Il nome del numero "+i+ " è "+s);
-	}
+		System.out.println(c.getId());
+		System.out.println(c.getNome());
+		System.out.println(c.getCognome());
+		System.out.println(c.getSesso());
+		System.out.println(c.getComune_nascita());
+		System.out.println(c.getData_nascita());
+	}*/
 	
 	public void creacod(String nome, String cognome,String data , String comune, char sesso ){
 		int counter=0;
