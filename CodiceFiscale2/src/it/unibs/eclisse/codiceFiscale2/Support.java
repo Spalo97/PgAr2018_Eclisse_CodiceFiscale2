6981729f1011a68469b1ceac47a6d4d828051161
Lange99 <c.langellotti@studenti.unibs.it>
2019-04-17 09:19:23 +0200

    inizio algoritmo di calcolo del codice fiscale

--- CodiceFiscale2/src/it/unibs/eclisse/codiceFiscale2/Support.java
+++ CodiceFiscale2/src/it/unibs/eclisse/codiceFiscale2/Support.java
@@ -26,6 +26,10 @@ public class Support {
 	private char sesso;
 	private String comune_nascita;
 	private String data_nascita;
+	private int anno;
+	private int mese;
+	private int giorno;
+	private String cod_fisc;
  	
 	private Persona p=new Persona();
 	
@@ -133,5 +137,122 @@ public class Support {
 		Persona c=persone.get(i);
 		String s= c.getNome();
 		System.out.println("Il nome del numero "+i+ " è "+s);
+	}
+	
+	public void creacod(String nome, String cognome,String data , String comune, char sesso ){
+		int counter=0;
+		nome_cognome(cognome);
+		nome_cognome(nome);
+		data_nascita(data);
+		cod_data(anno, mese, giorno, sesso);
+		
+	}
+	
+	public void nome_cognome(String dato){
+		int i=0;
+		int counter=0;
+		do {
+			if(dato.substring(i) != "A" && dato.substring(i) != "E" && dato.substring(i) != "I" && dato.substring(i) != "O" && dato.substring(i) != "U" ) {
+				cod_fisc=cod_fisc+dato.substring(i);
+				counter ++;
+			}
+			i++;
+		}while(counter<3 || i<dato.length());
+		i=0;
+		if (counter<3) {
+			do {
+				if(dato.substring(i) == "A" && dato.substring(i) == "E" && dato.substring(i) == "I" && dato.substring(i) == "O" && dato.substring(i) == "U" ) {
+					cod_fisc=cod_fisc+dato.substring(i);
+					counter ++;
+				}
+				i++;
+			}while(counter<3 || i<dato.length());
+		}
+		if(counter<3) {
+			do {
+				cod_fisc=cod_fisc+"X";
+				counter ++;
+			}while(counter<3);
+		}
+	}
+	
+	
+	public void data_nascita(String data) {
+
+		anno=Integer.parseInt(data.substring(0, 4));
+		mese=Integer.parseInt(data.substring(6, 7));
+		giorno=anno=Integer.parseInt(data.substring(9, 10));
+	}
+	
+	public void cod_data(int anno, int mese, int giorno, char sesso) {
+		int anno2c=anno%100;
+		int giornilimite;
+		//aggiunta dello 0 davanti ai numeri <10, Es: nati nel 2000
+		
+		
+		switch(mese){
+		case 1:
+			cod_fisc=cod_fisc+"A";;
+			giornilimite=31;
+			break;
+		case 2:
+			cod_fisc=cod_fisc+"B";
+			giornilimite=28;
+			break;
+		case 3:
+			cod_fisc=cod_fisc+"C";
+			giornilimite=31;
+			break;
+		case 4:
+			cod_fisc=cod_fisc+"D";
+			giornilimite=30;
+			break;
+		case 5:
+			cod_fisc=cod_fisc+"E";
+			giornilimite=31;
+			break;
+		case 6:
+			cod_fisc=cod_fisc+"H";
+			giornilimite=30;
+			break;
+		case 7:
+			cod_fisc=cod_fisc+"L";
+			giornilimite=31;
+			break;
+		case 8:
+			cod_fisc=cod_fisc+"M";
+			giornilimite=31;
+			break;
+		case 9:
+			cod_fisc=cod_fisc+"P";
+			giornilimite=30;
+			break;
+		case 10:
+			cod_fisc=cod_fisc+"R";
+			giornilimite=31;
+			break;
+		case 11:
+			cod_fisc=cod_fisc+"S";
+			giornilimite=30;
+			break;
+		case 12:
+			cod_fisc=cod_fisc+"T";
+			giornilimite=31;
+			break;
+		default:
+			cod_fisc=cod_fisc+"";
+			break;
+		}
+		if (sesso == 'F') {
+			giornilimite=+40;
+			if(giorno>giornilimite) {
+				//errore
+			} else cod_fisc=cod_fisc+(giorno+40);		
+		}
+		else {
+			if(giorno>giornilimite) {
+				//errore
+			} else cod_fisc=cod_fisc+giorno;
+		}	
 	}
 }
